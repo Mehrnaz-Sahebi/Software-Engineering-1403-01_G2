7d5fa47e7d3de2a2f58f8ac4d7815ad22f3a859b
parsa333111 <hashemikhorsand6556@gmail.com>
2024-12-30 13:12:36 +0330

    Save word probabilities in database addes

--- src/group10/create_dataset.py
+++ src/group10/create_dataset.py
@@ -4,11 +4,12 @@ from tqdm import tqdm
 from collections import defaultdict
 
 normalizer = Normalizer()
+normalizer_english = Normalizer(pinglish_conversion_needed=True)
 tokenizer = Tokenizer()
 stemmer = FindStems()
 
 def normalize(text):
-    return normalizer.normalize(text)
+    return normalizer.normalize(normalizer_english.normalize(text))
 
 def findStem(tokens):
     return [stemmer.convert_to_stem(token) for token in tokens]
@@ -32,11 +33,51 @@ def count_frequency(tokens):
 
 def create_dataset(dataset):
     dataset_size = len(dataset['train'])
+
     for index in tqdm(range(dataset_size)):
         text = dataset['train'][index]['Text']
         tokens = create_tokens_from_text(text)
         count_frequency(tokens)
 
+def compute_probabilities(frequency_counter):
+    probabilities = {}
+    word_counts = defaultdict(int)
+
+    for (past_word, current_word), count in frequency_counter.items():
+        word_counts[past_word] += count
+
+    for (past_word, current_word), count in frequency_counter.items():
+        probabilities[(past_word, current_word)] = count / word_counts[past_word]
+    
+    return probabilities
+
+def save_to_database(probabilities, db_name="TBA"):
+    conn = "TBA"
+    cursor = conn.cursor()
+    
+    cursor.execute(
+        """
+            CREATE TABLE IF NOT EXISTS G10_word_probabilities (
+                past_word TEXT,
+                current_word TEXT,
+                probability REAL,
+                PRIMARY KEY (past_word, current_word)
+            )
+        """
+        )
+    
+    for (past_word, current_word), probability in probabilities.items():
+        cursor.execute(
+        """
+            INSERT OR REPLACE INTO G10_word_probabilities (past_word, current_word, probability)
+            VALUES (?, ?, ?)
+        """, (past_word, current_word, probability))
+    
+    conn.commit()
+    conn.close()
+
+
 dataset = load_dataset("codersan/Persian-Wikipedia-Corpus")
 create_dataset(dataset)
-print(dict(frequency_counter))
+probabilities = compute_probabilities(frequency_counter)
+save_to_database(probabilities, )
