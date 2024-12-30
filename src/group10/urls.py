5f5e21473ebbceac6ea8f1a4ff27759542e50213
skye <amirpax32@gmail.com>
2024-12-30 11:39:34 -0500

    add urls for next word suggestion.

--- src/group10/urls.py
+++ src/group10/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+
+import registration.views as registration
+
 from . import views
 
-app_name = 'group10'
+app_name = "group10"
 urlpatterns = [
-  path('', views.home, name='group10')
-
-] 
+    path("/nextword", views.home, name="home"),
+    path("/nextword/suggest", views.home, name="suggest"),
+    path("/nextword/signup/", registration.SignupPage, name="signup"),
+    path("/nextword/login/", registration.LoginPage, name="login"),
+    path("/nextword/logout/", registration.LogoutPage, name="logout"),
+]
--- src/group10/views.py
+++ src/group10/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render (request , 'group10.html' , {'group_number': '10'})
 
+def home(request):
+    return render(request, "group10.html", {"group_number": "10"})
+
+
+def suggest(request):
+    return render(request, "group10.html", {"group_number": "10"})
