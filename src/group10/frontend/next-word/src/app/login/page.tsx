3f7da3e459e36eff2a2b5893a4865e7e19a48c37
mirnajafizade <mirnajafizade@mahsan.co>
2025-01-03 21:31:54 +0330

    add FormRaw component and refactor form part of login and signup.

--- src/group10/frontend/next-word/src/app/login/page.tsx
+++ src/group10/frontend/next-word/src/app/login/page.tsx
@@ -1,33 +1,14 @@
 import React from "react";
 import LinkLabel from "@/components/LinkLabel";
+import FormRaw from "@/components/FormRaw";
 
 const LoginPage: React.FC = () => {
     return (
-        <div className="bg-white p-8 rounded-lg shadow-lg w-96">
+        <div>
             <h2 className="m-4 text-2xl font-bold text-center mb-6">Login</h2>
             <form>
-                <div className="mb-4">
-                    <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
-                        Username
-                    </label>
-                    <input
-                        id="username"
-                        type="text"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your username"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
-                        Password
-                    </label>
-                    <input
-                        id="password"
-                        type="password"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your password"
-                    />
-                </div>
+                <FormRaw htmlFor="username" label="Username" id="username" type="text"/>
+                <FormRaw htmlFor="username" label="Password" id="password" type="password"/>
                 <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200"
--- src/group10/frontend/next-word/src/app/signup/page.tsx
+++ src/group10/frontend/next-word/src/app/signup/page.tsx
@@ -1,78 +1,18 @@
 import React from "react";
 import LinkLabel from "@/components/LinkLabel";
+import FormRaw from "@/components/FormRaw";
 
 const SignupPage: React.FC = () => {
     return (
-        <div className="m-4 bg-white p-8 rounded-lg shadow-lg w-96">
+        <div>
             <h2 className="text-2xl font-bold text-center mb-6">Signup</h2>
             <form>
-                <div className="mb-4">
-                    <label htmlFor="username" className="block text-gray-700 font-medium mb-2">
-                        Username
-                    </label>
-                    <input
-                        id="username"
-                        type="text"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your username"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="email" className="block text-gray-700 font-medium mb-2">
-                        Email
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:rong-2 focus:ring-blue-500"
-                        placeholder="Enter your email"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
-                        Password
-                    </label>
-                    <input
-                        id="password"
-                        type="password"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your password"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="password" className="block text-gray-700 font-medium mb-2">
-                        Confirm Password
-                    </label>
-                    <input
-                        id="password2"
-                        type="password"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your password"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="name" className="block text-gray-700 font-medium mb-2">
-                        Name
-                    </label>
-                    <input
-                        id="name"
-                        type="text"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your name"
-                    />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="age" className="block text text-gray-700 font-medium mb-2">
-                        Age
-                    </label>
-                    <input
-                        id="name"
-                        type="number"
-                        min="0"
-                        className="w-full border border-gray-300 px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        placeholder="Enter your age"
-                    />
-                </div>
+                <FormRaw htmlFor="username" label="Username" id="username" type="text"/>
+                <FormRaw htmlFor="email" label="Email" id="email" type="email"/>
+                <FormRaw htmlFor="password" label="Password" id="password" type="password"/>
+                <FormRaw htmlFor="password" label="Confirm Password" id="password2" type="password"/>
+                <FormRaw htmlFor="name" label="Name" id="name" type="text"/>
+                <FormRaw htmlFor="age" label="Age" id="name" type="number"/>
                 <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-200"
@@ -80,6 +20,7 @@ const SignupPage: React.FC = () => {
                     Signup
                 </button>
             </form>
+            <LinkLabel text="Already have an account?" link="/login" linkText="Login"/>
             <LinkLabel text="Do not want to signup?" link="/" linkText="Home"/>
         </div>
     );
