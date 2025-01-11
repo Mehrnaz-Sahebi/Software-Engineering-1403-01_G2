bc2c8761415914109337838e99c8d099afae23a0
Arvin Amirhosseini <153392036+Arvin-amh@users.noreply.github.com>
2025-01-11 20:21:45 +0330

    Update urls.py

--- src/group8/urls.py
+++ src/group8/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-
-
-app_name = 'group8'
+app_name = 'group8'  
 urlpatterns = [
-  path('', views.home, name='group8')
-
-] 
+    path('', views.home, name='group8'),
+    path('login/', views.login, name='login'),
+    path('submit-text/', views.submit_text, name='submit_text'),
+]
