3bca2196c7efae1d9a2c56a342c833f92c7ff608
skye <amirpax32@gmail.com>
2025-01-13 14:01:24 -0500

    fix nexjs build and connection to django

--- src/group10/urls.py
+++ src/group10/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
+from django.views.static import serve
+
+from FarsiAid_website.settings import STATICFILES_DIRS
 
 from . import views
 
 app_name = "group10"
 urlpatterns = [
-    path("", views.HomePage, name="home"),
-    path("suggest/", views.Suggest, name="suggest"),
-    path("signup/", views.SignupPage, name="signup"),
-    path("login/", views.LoginPage, name="login"),
-    path("logout/", views.LogoutPage, name="logout"),
+    path("api/suggest/", views.suggest_api, name="suggest"),
+    path("api/csrf/", views.csrf_api, name="csrf"),
+    path("api/signup/", views.signup_api, name="signup"),
+    path("api/login/", views.login_api, name="login"),
+    path("api/logout/", views.logout_api, name="logout"),
+    path("<path:path>", serve, {"document_root": STATICFILES_DIRS[0]}),
 ]
