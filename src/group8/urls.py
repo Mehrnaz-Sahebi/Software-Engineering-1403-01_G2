006ddb050da42880829b9645a752c06ea1977ba7
moeinEN <moenayatti@gmail.com>
2025-01-15 04:30:51 +0330

    complete urls.py group8 to redirect correctly

--- src/group8/urls.py
+++ src/group8/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'group8'  
 urlpatterns = [
-    path('', views.home, name='group8'),
+    path('', views.home, name='home'),
     path('home/', views.home, name='group8'),
     path('login/', views.login, name='login'),
     path('submit-text/', views.submit_text, name='submit_text'),
